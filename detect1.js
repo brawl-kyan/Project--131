b16544e4198147d7f27441b8d9b861815c246924
brawl-kyan <79627900+brawl-kyan@users.noreply.github.com>
2022-07-14 18:14:48 +0530

    Add files via upload

---
+++ detect1.js
@@ -0,0 +1,24 @@
+stat = "";
+function preload(){
+img= loadImage("download.jpg");    
+}
+
+function setup(){
+canvas = createCanvas(400,400);
+canvas.position(450,120);
+objectDetecter = ml5.objectDetector('cocossd',modalLoaded);
+}
+function draw(){
+image(img,0,0,400,400)    
+}
+function modalLoaded(){
+    
+stat = true;
+objectDetecter.detect(img , gotResults);    
+}
+function gotResults(error,results){
+if(error){
+console.log(error);    
+}
+console.log(results)
+}
